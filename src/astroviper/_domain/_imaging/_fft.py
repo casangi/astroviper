23ed63ad097c4e1483477ac8e00c8979243ec80f
Dave Mehringer <dmehring@nrao.edu>
2024-01-27 16:29:20 -0500

    updates

--- src/astroviper/_domain/_imaging/_fft.py
+++ src/astroviper/_domain/_imaging/_fft.py
@@ -3,7 +3,7 @@ from typing import List
 
 def _fft_lm_to_uv(ary: np.ndarray, axes: List[int]) -> np.ndarray:
     """
-    Do a 2-D FFT
+    Do a 2-D FFT. This returns a complex-valued array
     :ary : np.ndarray
         Numpy array to FT
     :axes : List[int]
