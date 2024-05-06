7428d522a185e936d4ccc4df6f4a01d9bf6dd831
Srikrishna Sekhar <ssekhar@nrao.edu>
2024-05-06 16:06:27 -0600

    gridflag : intermediate commit
    The graph building works and runs through from end to end, but the function
    calls still yield the wrong results. The intention was to generate a histogram
    per uv-pixel and use that histogram to calculate the median and MAD per pixel.
    However at the moment it simply accumulates all the points per pixel before
    calculation which becomes untenable for large datasets.

--- src/astroviper/flagging/grid_flag.py
+++ src/astroviper/flagging/grid_flag.py
@@ -1,17 +1,28 @@
 def grid_flag(ps_store, grid_flag_params):
+
     from xradio.vis.read_processing_set import read_processing_set
-    from graphviper.graph_tools.coordinate_utils import make_parallel_coord
+
+    from graphviper.graph_tools import map, reduce, generate_dask_workflow
+    from graphviper.graph_tools.coordinate_utils import (
+        make_parallel_coord,
+        interpolate_data_coords_onto_parallel_coords)
+
     from astroviper._domain._flagging._gridflag_histogram import (
         compute_uv_histogram,
         merge_uv_grids,
-        apply_flags,
-        generate_flags,
-    )
-    from graphviper.graph_tools import map, reduce
+        apply_flags)
+
     import dask
+    import numpy as np
+
+    # ----
 
     ps = read_processing_set(ps_store)
-    print(ps.summary())
+
+    for msv4 in ps:
+        nflag = np.count_nonzero(ps[msv4].FLAG)
+        frac = nflag/ps[msv4].FLAG.size
+        print(f"{msv4} % flags {frac*100.:.3f}")
 
     # uvrange = [0,12500]
     # uvcell = 10
@@ -24,51 +35,55 @@ def grid_flag(ps_store, grid_flag_params):
     input_params = grid_flag_params
     input_params["npixels"] = [npixu, npixv]
     input_params["input_data_store"] = ps_store
+    input_params["nhistbin"] = grid_flag_params['nhistbin']
 
     # Make Parallel Coords.
     # Needs to be reworked for multiple spw.
-    # Development: Only selecting the first 5 channels so it does not take too long.
-    n_chunks = 5
+    n_chunks = 12
     parallel_coords = {}
     ms_xds = ps.get(0)
     parallel_coords["frequency"] = make_parallel_coord(
-        coord=ms_xds.frequency[0:5], n_chunks=n_chunks
-    )
-    print(len(parallel_coords["frequency"]["data"]))
-
-    from graphviper.graph_tools.coordinate_utils import (
-        interpolate_data_coords_onto_parallel_coords,
+        coord=ms_xds.frequency, n_chunks=n_chunks
     )
 
     node_task_data_mapping = interpolate_data_coords_onto_parallel_coords(
         parallel_coords, ps
     )
 
-    # Create Map Graph
+    # Generate map graph
     graph = map(
         input_data=ps,
         node_task_data_mapping=node_task_data_mapping,
         node_task=compute_uv_histogram,
-        input_parms=input_params,
+        input_params=input_params,
         in_memory_compute=False,
     )
 
-    # input_parms = {}
-    graph = reduce(graph, merge_uv_grids, input_params, mode="single_node")
+    # Generate reduce graph
+    graph = reduce(graph, merge_uv_grids, input_params, mode="tree")
+    dask_graph = generate_dask_workflow(graph)
 
     input_params = {}
-    input_params["accum_uv_hist"] = graph
+    input_params["accum_uv_hist"] = dask_graph
 
     graph = map(
         input_data=ps,
         node_task_data_mapping=node_task_data_mapping,
-        node_task=generate_flags,
-        input_parms=input_params,
+        node_task=apply_flags,
+        input_params=input_params,
         in_memory_compute=False,
     )
 
+    dask_graph = generate_dask_workflow(graph)
     # Inspect graph.
-    dask.visualize(graph, filename="gridflag_graph.png")
+    dask.visualize(dask_graph, filename="gridflag_graph_raw.png", ranked='LR')
 
-    result = dask.compute(graph)
-    print(result)
+    result = dask.compute(dask_graph)
+    for msv4 in ps:
+        nflag = np.count_nonzero(ps[msv4].FLAG)
+        frac = nflag/ps[msv4].FLAG.size
+        print(f"{msv4} % flags {frac*100.:.3f}")
+
+    print("DONE")
+
+    return graph
