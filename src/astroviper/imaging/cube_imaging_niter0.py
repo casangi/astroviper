dd0a4f947b09f6008f68174d1b43e52cbdf0166a
Andrew Thomas <amcnicho@nrao.edu>
2024-07-18 12:42:19 -0400

    Apply black formatting

--- src/astroviper/imaging/cube_imaging_niter0.py
+++ src/astroviper/imaging/cube_imaging_niter0.py
@@ -5,17 +5,17 @@ def cube_imaging_niter0(
     ps_name,
     image_name,
     grid_params,
-    polarization_coord=None, 
-    frequency_coord=None, 
+    polarization_coord=None,
+    frequency_coord=None,
     n_chunks=None,
     data_variables=["sky", "point_spread_function", "primary_beam"],
     intents=["OBSERVE_TARGET#ON_SOURCE"],
     compressor=Blosc(cname="lz4", clevel=5),
-    data_group='base',
+    data_group="base",
     double_precission=True,
     thread_info=None,
-    workflow_ochestrator:{'dask','airflow'}='dask',
-    airflow_dag_folder=None
+    workflow_ochestrator: {"dask", "airflow"} = "dask",
+    airflow_dag_folder=None,
 ):
     import numpy as np
     import xarray as xr
@@ -53,7 +53,7 @@ def cube_imaging_niter0(
     if polarization_coord is None:
         polarization_coord = ps.get(0).polarization
 
-    #Create empty image
+    # Create empty image
     img_xds = make_empty_sky_image(
         phase_center=grid_params["phase_direction"]["data"],
         image_size=grid_params["image_size"],
@@ -65,42 +65,83 @@ def cube_imaging_niter0(
     write_image(img_xds, imagename=image_name, out_format="zarr")
 
     if n_chunks is None:
-        #Calculate n_chunks
+        # Calculate n_chunks
         from astroviper._utils.data_partitioning import bytes_in_dtype
+
         ## Determine the amount of memory required by the node task if all dimensions that chunking will occur on are singleton.
         ## For example cube_imaging does chunking only only frequency, so memory_singleton_chunk should be the amount of memory requered by _feather when there is a single frequency channel.
 
-        n_pixels_single_frequency = img_xds.sizes['l']*img_xds.sizes['m']*img_xds.sizes['polarization']*img_xds.sizes['time']
-        fudge_factor=1.2
+        n_pixels_single_frequency = (
+            img_xds.sizes["l"]
+            * img_xds.sizes["m"]
+            * img_xds.sizes["polarization"]
+            * img_xds.sizes["time"]
+        )
+        fudge_factor = 1.2
         if double_precission:
-            memory_singleton_chunk = fudge_factor*(3*n_pixels_single_frequency*bytes_in_dtype['complex64']/(1024**3) + 3*n_pixels_single_frequency*bytes_in_dtype['float32']/(1024**3))
+            memory_singleton_chunk = fudge_factor * (
+                3 * n_pixels_single_frequency * bytes_in_dtype["complex64"] / (1024**3)
+                + 3 * n_pixels_single_frequency * bytes_in_dtype["float32"] / (1024**3)
+            )
         else:
-            memory_singleton_chunk = fudge_factor*(3*n_pixels_single_frequency*bytes_in_dtype['complex128']/(1024**3) + 3*n_pixels_single_frequency*bytes_in_dtype['float64']/(1024**3))
+            memory_singleton_chunk = fudge_factor * (
+                3 * n_pixels_single_frequency * bytes_in_dtype["complex128"] / (1024**3)
+                + 3 * n_pixels_single_frequency * bytes_in_dtype["float64"] / (1024**3)
+            )
 
-        logger.debug("Memory required for a single frequency channel: " + str(memory_singleton_chunk) + " GiB")
+        logger.debug(
+            "Memory required for a single frequency channel: "
+            + str(memory_singleton_chunk)
+            + " GiB"
+        )
 
-        chunking_dims_sizes = {'frequency':img_xds.sizes['frequency']} #Need to know how many frequency channels there are.
-        from astroviper._utils.data_partitioning import calculate_data_chunking, get_thread_info
+        chunking_dims_sizes = {
+            "frequency": img_xds.sizes["frequency"]
+        }  # Need to know how many frequency channels there are.
+        from astroviper._utils.data_partitioning import (
+            calculate_data_chunking,
+            get_thread_info,
+        )
+
         if thread_info is None:
             thread_info = get_thread_info()
             logger.debug("Thread info " + str(thread_info))
-        n_chunks = calculate_data_chunking(memory_singleton_chunk, chunking_dims_sizes, thread_info, constant_memory=0, tasks_per_thread=4)['frequency']
-        logger.info('Number of frequency chunks: ' + str(n_chunks) + ' frequency channels: ' + str(chunking_dims_sizes))
+        n_chunks = calculate_data_chunking(
+            memory_singleton_chunk,
+            chunking_dims_sizes,
+            thread_info,
+            constant_memory=0,
+            tasks_per_thread=4,
+        )["frequency"]
+        logger.info(
+            "Number of frequency chunks: "
+            + str(n_chunks)
+            + " frequency channels: "
+            + str(chunking_dims_sizes)
+        )
 
-
-    #Make Parallel Coords
+    # Make Parallel Coords
     parallel_coords = {}
     parallel_coords["frequency"] = make_parallel_coord(
         coord=frequency_coord, n_chunks=n_chunks
     )
-    logger.info('Number of frequency chunks: ' + str(len(parallel_coords['frequency']['data_chunks'])))
+    logger.info(
+        "Number of frequency chunks: "
+        + str(len(parallel_coords["frequency"]["data_chunks"]))
+    )
 
-    from xradio.image._util._zarr.zarr_low_level import create_data_variable_meta_data_on_disk
+    from xradio.image._util._zarr.zarr_low_level import (
+        create_data_variable_meta_data_on_disk,
+    )
 
     if double_precission:
-        from xradio.image._util._zarr.zarr_low_level import image_data_variables_and_dims_double_precision as image_data_varaibles_and_dims
+        from xradio.image._util._zarr.zarr_low_level import (
+            image_data_variables_and_dims_double_precision as image_data_varaibles_and_dims,
+        )
     else:
-        from xradio.image._util._zarr.zarr_low_level import image_data_variables_and_dims_single_precision as image_data_varaibles_and_dims
+        from xradio.image._util._zarr.zarr_low_level import (
+            image_data_variables_and_dims_single_precision as image_data_varaibles_and_dims,
+        )
 
     xds_dims = dict(img_xds.dims)
     data_varaibles_and_dims_sel = {
@@ -122,41 +163,56 @@ def cube_imaging_niter0(
     input_parms["time"] = [0]
     input_parms["compressor"] = compressor
     input_parms["image_file"] = image_name
-    input_parms["input_data_store"]=ps_name
+    input_parms["input_data_store"] = ps_name
     input_parms["data_group"] = data_group
 
-    from graphviper.graph_tools.coordinate_utils import interpolate_data_coords_onto_parallel_coords
-    node_task_data_mapping = interpolate_data_coords_onto_parallel_coords(parallel_coords, ps)
+    from graphviper.graph_tools.coordinate_utils import (
+        interpolate_data_coords_onto_parallel_coords,
+    )
 
-    #Create Map Graph
+    node_task_data_mapping = interpolate_data_coords_onto_parallel_coords(
+        parallel_coords, ps
+    )
+
+    # Create Map Graph
     viper_graph = map(
         input_data=ps,
         node_task_data_mapping=node_task_data_mapping,
         node_task=_make_image,
         input_params=input_parms,
-        in_memory_compute=False
+        in_memory_compute=False,
     )
 
     input_params = {}
 
-    if workflow_ochestrator == 'dask':
-        viper_graph = reduce(viper_graph, _combine_return_data_frames, input_params, mode="tree")
-        #Compute cube
+    if workflow_ochestrator == "dask":
+        viper_graph = reduce(
+            viper_graph, _combine_return_data_frames, input_params, mode="tree"
+        )
+        # Compute cube
         dask_graph = generate_dask_workflow(viper_graph)
-        #dask.visualize(dask_graph,filename='cube_imaging.png')
+        # dask.visualize(dask_graph,filename='cube_imaging.png')
         return_dict = dask.compute(dask_graph)[0]
         zarr.consolidate_metadata(image_name)
         return return_dict
-    elif workflow_ochestrator == 'airflow':
-        viper_graph = reduce(viper_graph, _combine_return_data_frames, input_params, mode="single_node")
-        generate_airflow_workflow(viper_graph,dag_id='0',schedule_interval=None,filename=os.path.join(airflow_dag_folder,'cube_imaging_airflow.py'),dag_name='cube_imaging')
-    
-    
+    elif workflow_ochestrator == "airflow":
+        viper_graph = reduce(
+            viper_graph, _combine_return_data_frames, input_params, mode="single_node"
+        )
+        generate_airflow_workflow(
+            viper_graph,
+            dag_id="0",
+            schedule_interval=None,
+            filename=os.path.join(airflow_dag_folder, "cube_imaging_airflow.py"),
+            dag_name="cube_imaging",
+        )
 
-    #return parallel_coords
+    # return parallel_coords
+
 
 def _combine_return_data_frames(input_data, input_parms):
     import pandas as pd
+
     combined_df = pd.DataFrame()
 
     for df in input_data:
