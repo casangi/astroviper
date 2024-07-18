cfbe403e9057d1f0f2ebae6142647a09bd518664
Andrew Thomas <amcnicho@nrao.edu>
2024-07-18 15:18:12 -0400

    Add proper cleanup using pytest.fixture and another test case

--- tests/domain/imaging/test_cube_imaging_niter0.py
+++ tests/domain/imaging/test_cube_imaging_niter0.py
@@ -1,12 +1,16 @@
+import pytest
 import numpy as np
-from xradio.vis.read_processing_set import read_processing_set
-from astroviper.imaging.cube_imaging_niter0 import cube_imaging_niter0
+import shutil
+import os
+from pandas import Index
 
 
-def test_cube_imaging_niter0():
+@pytest.fixture
+def antennae_from_s3():
+    from astroviper.imaging.cube_imaging_niter0 import cube_imaging_niter0
+    from xradio.vis.read_processing_set import read_processing_set
 
     ps_store = "s3://viper-test-data/Antennae_North.cal.lsrk.split.v3.vis.zarr"
-    ps = read_processing_set(ps_store, intents=["OBSERVE_TARGET#ON_SOURCE"])
     image_name = "Antennae_North_Cube.img.zarr"
     grid_params = {
         "image_size": [500, 500],
@@ -14,17 +18,32 @@ def test_cube_imaging_niter0():
         "fft_padding": 1.0,
         "phase_direction": 1,
     }
-    polarization_coord = ps["Antennae_North.cal.lsrk.split_0"].polarization
-    frequency_coord = ps["Antennae_North.cal.lsrk.split_0"].frequency
+    ps_name = "Antennae_North.cal.lsrk.split_0"
     n_chunks = None
     data_variables = ["sky", "point_spread_function", "primary_beam"]
 
-    cube_imaging_niter0(
+    ps = read_processing_set(ps_store, intents=["OBSERVE_TARGET#ON_SOURCE"])
+    polarization_coord = ps[ps_name].polarization
+    frequency_coord = ps[ps_name].frequency
+
+    output = cube_imaging_niter0(
         ps_store,
         image_name,
         grid_params,
         polarization_coord=polarization_coord,
         frequency_coord=frequency_coord,
-        n_chunks=None,
+        n_chunks=n_chunks,
         data_variables=data_variables,
     )
+    yield output
+
+    # cleanup
+    shutil.rmtree(image_name)
+
+
+def test_file_creation(antennae_from_s3):
+    assert os.path.exists("Antennae_North_Cube.img.zarr")
+
+
+def test_shape_matches(antennae_from_s3):
+    assert antennae_from_s3[0].shape == (8, 13)
