9bea6a8fd850ad97b70be637a7f87cd2c3fb7873
Des Small <small@jive.eu>
2025-02-12 14:43:41 +0100

    Added some type information to fringefit functions

--- src/astroviper/calibration/fringe_cal_quantum.py
+++ src/astroviper/calibration/fringe_cal_quantum.py
@@ -1,4 +1,5 @@
 from xradio.measurement_set.processing_set import ProcessingSet
+from xradio.measurement_set.measurement_set_xds import MeasurementSetXds
 
 import dask
 import numpy as np
@@ -17,8 +18,10 @@ def unique(s):
             u.append(c)
     return u
 
-def make_empty_cal_quantum(xds):
-    # time_c = xa.Coordinates(coords = {'time' : [0.5*(xds.time[0]+xds.time[-1])]})
+def make_empty_cal_quantum(xds: MeasurementSetXds):
+    """W
+args: xds, the measurement set used to get the axes we need for our calibration quantum
+    """
     pols_ant = unique(''.join([c for c in ''.join(xds.polarization.values)]))
     coord_dict = {
         'time' : [xds.time.values[0]],
@@ -59,11 +62,10 @@ class SingleFringeJones(object):
         """
 """
         self.q = q
-    def calc_antenna_jones(self, xds):
+    def calc_antenna_jones(self, xds: MeasurementSetXds):
         # We scrape all the metadata from an xds. Maybe this is wise, maybe not.
         frequency = xds.frequency
         ref_freq = self.q.frequency.values[0]
-        # These aren't the right values.
         dt = xds.time.values - self.q.time.values[0]
         df = frequency.values - ref_freq
         # We upscale the dimensions so that things broadcast nicely:
@@ -79,17 +81,25 @@ class SingleFringeJones(object):
         # Calculate phases: 
         phi = (phi0 + 2*np.pi*tau*df_shaped +
                2*np.pi*ref_freq*r*dt_shaped)
-        # breakpoint()
+        # Shape: (n_time, n_ant, n_freq, n_pol)
         antenna_jones = np.exp(1J*phi, dtype=complex)
         return antenna_jones
     def get_left_jones(self, antenna_jones, left_indices):
+        # antenna_jones has shape: (n_time, n_ant, n_freq, n_pol)
+        # We map the left antenna in a baseline to the baseline dimension
         jones_diags_left0 = antenna_jones[:, left_indices, :, :]
+        # We square up the polarization axis to (2,2) for ease of multiplication
+        # (n_time, n_baseline, n_freq, 2, 2)
         jones_diags_left = np.expand_dims(jones_diags_left0, (-1,)) * np.identity(2)
         return jones_diags_left
     def get_right_jones(self, antenna_jones, right_indices):
+        # antenna_jones has shape: (n_time, n_ant, n_freq, n_pol)
+        # We map the left antenna in each baseline to the baseline dimension
         jones_diags_right0 = antenna_jones[:, right_indices, :, :]
         jones_diags_right = np.expand_dims(jones_diags_right0, (-1,)) * np.identity(2)
-        # Swap the last two axes only
+        # We square up the polarization axis to (2,2) for ease of multiplication
+        # (n_time, n_baseline, n_freq, 2, 2)
+        # and take the hermitian conjugate over the last two axes:
         jones_diags_right = (np.transpose(jones_diags_right, axes=(0, 1, 2, 4, 3))).conj()
         return jones_diags_right
     def get_ant_to_baseline_maps(self, xds):
@@ -103,13 +113,14 @@ class SingleFringeJones(object):
     def apply_cal(self, xds):
         antenna_jones = self.calc_antenna_jones(xds)
         left_indices, right_indices = self.get_ant_to_baseline_maps(xds)
+        # v has shape (n_time, n_baseline, n_freq, 2, 2)
         v = square_up_last_dimension(xds.VISIBILITY.values)
-        pols_ant = self.q.polarization
-        # Corrected visibilities:
+        # Jones matrices for each visibility for left and right antennas on a baseline
         left_jones = self.get_left_jones(antenna_jones, left_indices)
         right_jones = self.get_right_jones(antenna_jones, right_indices)
+        # We have a very concise and pleasant form for calibration:
         v2 = left_jones @ v @ right_jones
-        # Pack this into a DataArray to match the xds
+        # Reshape the visibilities back to (n_time, n_baseline, n_freq, 4)
         s3 = v2.shape[:-2] + (4,)
         v2 = v2.reshape(s3)
         coords={'time' : xds.time,
@@ -120,7 +131,7 @@ class SingleFringeJones(object):
         new_xds = xds.assign({'FRINGEFIT' : da})
         return new_xds
        
-def apply_cal_ps(ps, res, unixtime, interval):
+def apply_cal_ps(ps: ProcessingSet, res: list[xa.DataArray], unixtime: float, interval: float):
     """I suspect this is the wrong API"""
     ps2 = ps.ms_sel(time=slice(unixtime, unixtime+interval)) 
     ps3 = {k : v for k, v in ps2.items() if len(v.time.values) != 0}
