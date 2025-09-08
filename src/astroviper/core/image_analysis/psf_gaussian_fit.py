Remove -90deg additional rotation in resultant position angle. Fixed the unit tests.

src/astroviper/core/image_analysis/psf_gaussian_fit.py
@@ -154,8 +154,9 @@ def psf_gaussian_fit_core(image_to_fit, npix_window, sampling, cutoff, delta):
 
                 #
                 # phi = res_x[2] - 90.0
-                # if phi < -90.0:
-                #    phi += 180.0
+                phi = res_x[2]
+                if phi < -90.0:
+                    phi += 180.0
 
                 ellipse_params[time, chan, pol, 0] = np.max(
                     np.abs(res_x[0:2])
@@ -165,7 +166,7 @@ def psf_gaussian_fit_core(image_to_fit, npix_window, sampling, cutoff, delta):
                     np.abs(res_x[0:2])
                     # ) * np.abs(delta[1] * 2.355 / sampling[1] / npix_window[1])
                 ) * np.abs(delta[1] * 2.355)
-                # ellipse_params[time, chan, pol, 2] = -phi
-                ellipse_params[time, chan, pol, 2] = -res_x[2]
+                ellipse_params[time, chan, pol, 2] = -phi
+                # ellipse_params[time, chan, pol, 2] = -res_x[2]
                 print("res_x=", res_x)
     return ellipse_params

tests/unit/core/image_analysis/test_psf_gaussian_fit.py
@@ -48,7 +48,7 @@ def test_psf_gaussian_fit_basic():
     params = result["BEAM"]["beam_param"]
     assert params.shape[-1] == 3
     # Check that the fitted widths are positive
-    assert np.all(result["BEAM"].data > 0)
+    assert np.all(result["BEAM"].data[:, :, :-1] > 0)
 
 
 def test_psf_gaussian_fit_custom_window():
@@ -56,4 +56,4 @@ def test_psf_gaussian_fit_custom_window():
     result = psf_gaussian_fit(test_dataset, npix_window=[7, 7], sampling=[7, 7])
     params = result["BEAM"]["beam_param"]
     assert params.shape == (3,)
-    assert np.all(result["BEAM"].data > 0)
+    assert np.all(result["BEAM"].data[:, :, :-1] > 0)