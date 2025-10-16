5bf406974576042bcd78cf84c5424d0a3966647b
Kumar Golap <kgolap@nrao.edu>
2025-10-16 17:18:58 -0600

    check point of development

--- src/astroviper/core/imaging/imaging_utils/standard_gridding_example.py
+++ src/astroviper/core/imaging/imaging_utils/standard_gridding_example.py
@@ -1,4 +1,3 @@
-import xradio
 import numpy as np
 import xarray as xr
 import os
@@ -6,11 +5,18 @@ from toolviper.utils.data import download
 from astropy import units as u
 from astropy import constants as const
 from matplotlib import pyplot as pl
-from astroviper.core.imaging.imaging_utils.standard_grid import *
+from astroviper.core.imaging.imaging_utils.gcf_prolate_spheroidal import (
+    create_prolate_spheroidal_kernel_1D as sph_1D,
+    create_prolate_spheroidal_kernel as sph_2D,
+)
+from astroviper.core.imaging.imaging_utils.standard_grid import (
+    standard_grid_numpy_wrap,
+    grid2image_spheroid_ms4,
+)
 from astroviper.core.imaging.imaging_utils.standard_degrid import (
     degrid_spheroid_ms4,
 )
-from astroviper.core.imaging.imaging_utils.gcf_prolate_spheroidal import *
+
 from astroviper.core.imaging.fft import fft_lm_to_uv
 
 
@@ -60,9 +66,7 @@ def generate_ms4_with_point_sources(
     ] = sources[2]
     # convolve it with a gaussian of 5 pixels
     # mod_im = gaussian_filter(mod_im, sigma=5)
-    mod_im = np.pad(
-        mod_im, pad_width=((1000, 1000), (1000, 1000)), constant_values=0.0
-    )
+    mod_im = np.pad(mod_im, pad_width=((1000, 1000), (1000, 1000)), constant_values=0.0)
     ft_mod = fft_lm_to_uv(mod_im, axes=[0, 1])
 
     uv_components = origms_subset.UVW.sel(uvw_label=["u", "v"])
@@ -71,9 +75,7 @@ def generate_ms4_with_point_sources(
     # Sum the squares along the uvw_label dimension
     sum_square_uv = uv_squared.sum(dim="uvw_label")
     max_uvdist = np.sqrt(sum_square_uv.max()).values
-    cell = ((0.5 / ((invlam).to(u.Unit("/m")).value * max_uvdist)) * u.rad).to(
-        "arcsec"
-    )
+    cell = ((0.5 / ((invlam).to(u.Unit("/m")).value * max_uvdist)) * u.rad).to("arcsec")
     ny, nx = ft_mod.shape
 
     x_axis = np.linspace(-max_uvdist, max_uvdist, nx)
@@ -115,12 +117,8 @@ def generate_ms4_with_point_sources(
     for t in range(num_time):
         for b in range(num_baseline):
             # Get the 'u' and 'v' coordinate for the current time and baseline
-            current_u = (
-                uv_coords.isel(time=t, baseline_id=b).sel(uvw_label="u").values
-            )
-            current_v = (
-                uv_coords.isel(time=t, baseline_id=b).sel(uvw_label="v").values
-            )
+            current_u = uv_coords.isel(time=t, baseline_id=b).sel(uvw_label="u").values
+            current_v = uv_coords.isel(time=t, baseline_id=b).sel(uvw_label="v").values
 
             # Handle potential NaN values in UVW coordinates
             if np.isnan(current_u) or np.isnan(current_v):
@@ -162,29 +160,23 @@ def make_standard_grid_image(dopsf=False):
 
     params = {}
     params["image_size_padded"] = np.array([npix, npix], dtype=int)
-    params["cell_size"] = np.array(
-        [cell.to("rad").value, cell.to("rad").value]
-    )
+    params["cell_size"] = np.array([cell.to("rad").value, cell.to("rad").value])
     params["complex_grid"] = True
     params["oversampling"] = 100
     params["support"] = 7
     params["do_psf"] = dopsf
     params["chan_mode"] = "continuum"
 
-    cgk_1D = create_prolate_spheroidal_kernel_1D(
-        params["oversampling"], params["support"]
-    )
+    cgk_1D = sph_1D(params["oversampling"], params["support"])
     grid, sumwgt = standard_grid_numpy_wrap(
         vis_data, uvw, weight, freq_chan, cgk_1D, params
     )
 
-    kernel, corrTerm = create_prolate_spheroidal_kernel(
+    kernel, corrTerm = sph_2D(
         params["oversampling"], params["support"], params["image_size_padded"]
     )
     dirty_im = (
-        np.real(
-            np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(grid[0, 0, :, :])))
-        )
+        np.real(np.fft.fftshift(np.fft.ifft2(np.fft.ifftshift(grid[0, 0, :, :]))))
         / corrTerm
         * npix
         * npix
@@ -228,18 +220,14 @@ def make_standard_grid_degrid():
 
     params = {}
     params["image_size_padded"] = np.array([npix, npix], dtype=int)
-    params["cell_size"] = np.array(
-        [cell.to("rad").value, cell.to("rad").value]
-    )
+    params["cell_size"] = np.array([cell.to("rad").value, cell.to("rad").value])
     params["complex_grid"] = True
     params["oversampling"] = 100
     params["support"] = 7
     params["do_psf"] = False
     params["chan_mode"] = "continuum"
 
-    cgk_1D = create_prolate_spheroidal_kernel_1D(
-        params["oversampling"], params["support"]
-    )
+    cgk_1D = sph_1D(params["oversampling"], params["support"])
     grid, sumwgt = standard_grid_numpy_wrap(
         vis_data, uvw, weight, freq_chan, cgk_1D, params
     )
@@ -258,7 +246,7 @@ def make_standard_grid_degrid():
     return ms4
 
 
-def make_standard_degrid_grid():
+def make_standard_degrid_grid(whichFunc=0):
     nsources = 4
     sources, npix, cell, ms4 = generate_ms4_with_point_sources(
         nsources, np.ones(nsources)
@@ -281,30 +269,22 @@ def make_standard_degrid_grid():
         support=7,
         sampling=100,
         incremental=False,
+        whichFunc=whichFunc,
     )
-    # Now lets grid and image model vis
-    vis_data = ms4.VISIBILITY_MODEL.data
-    uvw = ms4.UVW.data
-    dims = ms4.dims
-    weight = np.ones([dims["time"], dims["baseline_id"], dims["frequency"], 1])
-    freq_chan = ms4.coords["frequency"].values
 
-    params = {}
-    params["image_size_padded"] = np.array([npix, npix], dtype=int)
-    params["cell_size"] = np.array([incr, incr])
-    params["complex_grid"] = True
-    params["oversampling"] = 100
-    params["support"] = 7
-    params["do_psf"] = False
-    params["chan_mode"] = "continuum"
+    model_dirty = np.zeros([1, 2, npix, npix], dtype=float)
+    grid2image_spheroid_ms4(
+        vis=ms4,
+        resid_array=model_dirty,
+        pixelincr=np.array([incr, incr]),
+        support=7,
+        sampling=100,
+        dopsf=False,
+        column="VISIBILITY_MODEL",
+        chan_mode="continuum",
+    )
 
-    cgk_1D = create_prolate_spheroidal_kernel_1D(
-        params["oversampling"], params["support"]
-    )
-    grid, sumwgt = standard_grid_numpy_wrap(
-        vis_data, uvw, weight, freq_chan, cgk_1D, params
-    )
-    return grid, sources
+    return model_dirty, sources
 
 
 def plotvis_uvdist(vis, whichdata="VISIBILITY"):
