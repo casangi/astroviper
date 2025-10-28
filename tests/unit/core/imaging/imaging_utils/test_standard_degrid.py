19c0458aba7d7287ea99643c49b983760a48ad77
Kumar Golap <kgolap@nrao.edu>
2025-10-28 17:01:24 -0600

    blacked

--- tests/unit/core/imaging/imaging_utils/test_standard_degrid.py
+++ tests/unit/core/imaging/imaging_utils/test_standard_degrid.py
@@ -38,16 +38,10 @@ class TestStandardDegrid(unittest.TestCase):
         mod_im[sources[0], sources[1]] = sources[2]
         self.grid = np.zeros([nfreq, npol, npix, npix], dtype=complex)
         self.grid[0, 0, :, :] = np.conj(fft_lm_to_uv(mod_im, axes=[0, 1]))
-        self.vis_data = np.zeros(
-            (ntime, nant * nant, nfreq, npol), dtype=np.complex128
-        )
+        self.vis_data = np.zeros((ntime, nant * nant, nfreq, npol), dtype=np.complex128)
         self.uvw = np.zeros((ntime, nant * nant, 3), dtype=np.float64)
-        self.uvw[0, :, 0] = [
-            random.uniform(-maxUV, maxUV) for i in range(nant * nant)
-        ]
-        self.uvw[0, :, 1] = [
-            random.uniform(-maxUV, maxUV) for i in range(nant * nant)
-        ]
+        self.uvw[0, :, 0] = [random.uniform(-maxUV, maxUV) for i in range(nant * nant)]
+        self.uvw[0, :, 1] = [random.uniform(-maxUV, maxUV) for i in range(nant * nant)]
 
     def test_sgrid_basic(self):
         """Test sgrid with basic valid inputs."""
@@ -62,25 +56,17 @@ class TestStandardDegrid(unittest.TestCase):
         loc = np.zeros(3, dtype=int)
         off = np.zeros(3, dtype=int)
 
-        phasor = sgrid(
-            uvw, dphase, freq, c, scale, offset, sampling, pos, loc, off
-        )
+        phasor = sgrid(uvw, dphase, freq, c, scale, offset, sampling, pos, loc, off)
 
         # Expected values (calculate based on the sgrid logic)
         expected_pos_x = scale[0] * uvw[0] * freq / c + offset[0]
         expected_pos_y = scale[1] * uvw[1] * freq / c + offset[1]
         expected_loc_x = int(round(expected_pos_x))
         expected_loc_y = int(round(expected_pos_y))
-        expected_off_x = int(
-            round((expected_loc_x - expected_pos_x) * sampling)
-        )
-        expected_off_y = int(
-            round((expected_loc_y - expected_pos_y) * sampling)
-        )
+        expected_off_x = int(round((expected_loc_x - expected_pos_x) * sampling))
+        expected_off_y = int(round((expected_loc_y - expected_pos_y) * sampling))
         expected_phase = -2.0 * np.pi * dphase * freq / c
-        expected_phasor = complex(
-            np.cos(expected_phase), np.sin(expected_phase)
-        )
+        expected_phasor = complex(np.cos(expected_phase), np.sin(expected_phase))
 
         # Assertions
         self.assertAlmostEqual(phasor.real, expected_phasor.real)
@@ -105,25 +91,17 @@ class TestStandardDegrid(unittest.TestCase):
         offset = np.array([500.0, 500.0, 0.0])
         sampling = 1
 
-        phasor, loc, off = sgrid_numba(
-            uvw, dphase, freq, c, scale, offset, sampling
-        )
+        phasor, loc, off = sgrid_numba(uvw, dphase, freq, c, scale, offset, sampling)
 
         # Expected values (calculate based on the sgrid logic)
         expected_pos_x = scale[0] * uvw[0] * freq / c + offset[0]
         expected_pos_y = scale[1] * uvw[1] * freq / c + offset[1]
         expected_loc_x = int(round(expected_pos_x))
         expected_loc_y = int(round(expected_pos_y))
-        expected_off_x = int(
-            round((expected_loc_x - expected_pos_x) * sampling)
-        )
-        expected_off_y = int(
-            round((expected_loc_y - expected_pos_y) * sampling)
-        )
+        expected_off_x = int(round((expected_loc_x - expected_pos_x) * sampling))
+        expected_off_y = int(round((expected_loc_y - expected_pos_y) * sampling))
         expected_phase = -2.0 * np.pi * dphase * freq / c
-        expected_phasor = complex(
-            np.cos(expected_phase), np.sin(expected_phase)
-        )
+        expected_phasor = complex(np.cos(expected_phase), np.sin(expected_phase))
 
         # Assertions
         self.assertAlmostEqual(phasor.real, expected_phasor.real)
