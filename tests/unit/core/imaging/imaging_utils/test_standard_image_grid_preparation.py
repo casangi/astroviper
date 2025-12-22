932f44855f7a511f200ecdee510eca005fd97cde
Kumar Golap <kgolap@nrao.edu>
2025-12-22 13:43:47 -0700

    checkpoint with some functions padding

--- tests/unit/core/imaging/imaging_utils/test_standard_image_grid_preparation.py
+++ tests/unit/core/imaging/imaging_utils/test_standard_image_grid_preparation.py
@@ -1,7 +1,12 @@
 import numpy as np
 import xarray
 from xradio.image.image import make_empty_sky_image
-from astroviper.core.imaging.imaging_utils.standard_image_grid_preparation import *
+from astroviper.core.imaging.imaging_utils.standard_image_grid_preparation import (
+    _mult_div,
+    remove_padding,
+    apply_pb,
+    make_empty_padded_uv_image,
+)
 import unittest
 
 
@@ -18,16 +23,22 @@ class TestStandardImGridPrep(unittest.TestCase):
             phase_center=pc,
             image_size=imsize,
             cell_size=cellsize,
-            frequency_coords=freq,
+            chan_coords=freq,
             pol_coords=pol,
             time_coords=epoch,
         )
 
         sky_data_dims = ("time", "frequency", "polarization", "l", "m")
-        sky_data_shape = (len(epoch), len(freq), len(pol), imsize[0], imsize[1])
+        sky_data_shape = (
+            len(epoch),
+            len(freq),
+            len(pol),
+            imsize[0],
+            imsize[1],
+        )
         sky_coords = {dim: a.coords[dim] for dim in sky_data_dims}
         a["SKY"] = xarray.DataArray(
-            np.zeros(sky_data_shape, dtype=np.float32),
+            np.ones(sky_data_shape, dtype=np.float32),
             coords=sky_coords,
             dims=sky_data_dims,
         )
@@ -35,23 +46,57 @@ class TestStandardImGridPrep(unittest.TestCase):
 
     def test_remove_padding(self):
         a = self.make_image_xds()
-        ra_o, dec_o = (a.right_ascension[99, 99].data, a.declination[99, 99].data)
+        ra_o, dec_o = (
+            a.right_ascension[99, 99].data,
+            a.declination[99, 99].data,
+        )
         a = remove_padding(a, [180, 180])
-        ra_f, dec_f = (a.right_ascension[89, 89].data, a.declination[89, 89].data)
+        ra_f, dec_f = (
+            a.right_ascension[89, 89].data,
+            a.declination[89, 89].data,
+        )
         self.assertEqual(ra_f, ra_o)
         self.assertEqual(dec_f, dec_o)
+
+    ###################################
+    def test_make_empty_padded_uv_image(self):
+        a = self.make_image_xds()
+        b = make_empty_padded_uv_image(a, [250, 250], "RESIDUAL_VISIBILITY")
+        self.assertEqual(b.sizes["u"], 250)
+        print(
+            "SHAPES",
+            b["RESIDUAL_VISIBILITY"].shape,
+            b["RESIDUAL_VISIBILITY_NORMALIZATION"].shape,
+        )
 
     ##########################
     def test_mult_div(self):
 
         a = self.make_image_xds(npol=1, nchan=2)
-        a["SKY"] = 1  # set all values to 1
         sky_dims = a["SKY"].dims
         sky_coords = a["SKY"].coords
         sky_shape = a["SKY"].shape
         a["PRIMARY_BEAM"] = xarray.DataArray(
-            np.ones(sky_shape, dtype=np.float32), coords=sky_coords, dims=sky_dims
+            np.ones(sky_shape, dtype=np.float32),
+            coords=sky_coords,
+            dims=sky_dims,
         )
-        a["PRIMARY_BEAM"][0, 1, 0, :, :] = 0
-        _mult_div(a, multiply=True)
-        self.assertEqual(np.max(a["SKY"][0, 0, 0, :, :]), 1.0)
+        a["PRIMARY_BEAM"].data[0, 1, 0, :, :] = 0
+        _mult_div(a["SKY"], a["PRIMARY_BEAM"], multiply=True)
+        self.assertEqual(np.max(a["SKY"].data[0, 0, 0, :, :]), 1.0)
+
+    ######
+    def test_apply_pb(self):
+
+        a = self.make_image_xds(npol=1, nchan=2)
+        sky_dims = a["SKY"].dims
+        sky_coords = a["SKY"].coords
+        sky_shape = a["SKY"].shape
+        a["PRIMARY_BEAM"] = xarray.DataArray(
+            np.ones(sky_shape, dtype=np.float32),
+            coords=sky_coords,
+            dims=sky_dims,
+        )
+        a["PRIMARY_BEAM"].data[0, 1, 0, :, :] = 0
+        apply_pb(a, data_vars="SKY", multiply=True)
+        self.assertEqual(np.max(a["SKY"].data[0, 0, 0, :, :]), 1.0)
