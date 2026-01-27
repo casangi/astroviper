e8df4a22a5619cbda8e83072c2dbc6e2c01a6f51
Takahiro Tsutsumi <tak.tsutsumi@gmail.com>
2026-01-27 16:36:01 -0700

    Initial version of make_psf

---
+++ src/astroviper/core/imaging/imaging_utils/make_point_spread_function.py
@@ -0,0 +1,92 @@
+def make_psf(vis, im_params, grid_params):
+    """
+    Generate psf
+
+    Parameters:
+    ----------
+    vis : xarray.Dataset
+        The visibility data as an xarray Dataset.
+    im_params : dict
+        Imaging parameters: must include
+        'cell_size': angular size of a pixel 2d tuple (rad, rad),
+        'image_size': int 2d tuple (nx, ny),
+        'frequency_coords': list of frequencies in Hz,
+        'polarization': list of Stokes parameters,
+        'image_center': image center pixel coordinates int 2d tuple (x, y),
+        'phase_center': phase reference center (RA, Dec) in radians,
+        'time_coords': list of time coordinates in seconds.
+    grid_params : dict
+        Gridding parameters: must include
+        'sampling': sampling factor for gridding,
+        'complex_grid': boolean indicating if complex grid is used,
+        'support': support size for gridding.
+    Returns:
+        xarray.DataArray
+    """
+    from xradio.image import make_empty_sky_image
+    from astroviper.core.imaging.imaging_utils.gcf_prolate_spheroidal import (
+        create_prolate_spheroidal_kernel_1D,
+    )
+    from astroviper.core.imaging.imaging_utils.standard_grid import (
+        grid2image_spheroid_ms4,
+    )
+    import xarray as xr
+    import numpy as np
+
+    vis_data = vis.VISIBILITY.data
+    uvw = vis.UVW.data
+
+    # set weight to 1 for now
+    dims = vis.dims
+    freq_chan = vis.coords["frequency"].values
+    nfreq = len(freq_chan)
+    image_size = im_params["image_size"]
+    cell_size = im_params["cell_size"]
+    phase_center = im_params["phase_center"]
+    complex_grid = True
+    do_psf = True
+    # should be getting from im_params
+    chan_mode = im_params["chan_mode"]
+    sampling = grid_params["sampling"]
+    complex_grid = grid_params["complex_grid"]
+    support = grid_params["support"]
+    time_coords = vis.coords["time"].values[0]
+    pol = vis.coords["polarization"].values
+    npol = len(pol)
+    incr = cell_size[0]
+
+    psf_data = np.zeros([nfreq, npol, image_size[0], image_size[0]], dtype=float)
+    grid2image_spheroid_ms4(
+        vis=vis,
+        resid_array=psf_data,
+        pixelincr=np.array([-incr, incr]),
+        support=support,
+        sampling=sampling,
+        dopsf=True,
+        chan_mode=chan_mode,
+    )
+
+    psf_data_reshaped = np.expand_dims(psf_data, axis=0)
+    print("psf_data_reshaped.shape=", psf_data_reshaped.shape)
+
+    psf_xds = make_empty_sky_image(
+        phase_center=phase_center,
+        image_size=image_size,
+        cell_size=cell_size,
+        frequency_coords=freq_chan,
+        pol_coords=pol,
+        time_coords=time_coords,
+        direction_reference="fk5",
+        projection="SIN",
+        do_sky_coords=True,
+    )
+    new_dims = tuple(d for d in psf_xds.dims if d != "beam_params_label")
+    coords = psf_xds.drop_vars("beam_params_label").coords
+    psf_da = xr.DataArray(
+        psf_data_reshaped,
+        dims=new_dims,
+        coords=coords,
+        name="POINT_SPREAD_FUNCTION",
+    )
+
+    return psf_da
